44958a0149ea7d77a673bd88ead52ea03383b783
Michael 'Zen' Boeni <zen@eistengaming.com>
2020-05-19 14:54:54 +0200

    [Modified] More updates on the about page.

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -52,7 +52,7 @@ export default {
         id: util.createUUIDv4(),
         image: 'https://pbs.twimg.com/profile_banners/1228420911734366211/1588544940/600x200',
         aboutText: 'If you enjoy the software, you are welcome to donate: HERE (TBD)',
-        changelog: '<p style="color: #76FF03"><strong>NEW FEATURES</strong></p><p>* Analytics Dashboard: Top Games <br/>* Analytics Dashboard: Top Streams<br/>* Analytics Dashboard: Featured Streams<br/></p><p style="color: #FF7043"><strong>FIXES</strong></p>* The link to the "About" should no longer be removed when dev mode is disabled in the settings</br>* Box shots and for channel previews should now be displayed properly'
+        changelog: '<p style="color: #76FF03"><strong>NEW FEATURES</strong></p><p>* Added Top Games to the analytics dashboard <br/>* Added Top Streams to the analytics dashboard<br/>* Added Featured Streams to the analytics dashboard<br/>* Added Streamer Info to the analytics dashboard<br/></p><p style="color: #FF7043"><strong>FIXES</strong></p>* The link to the "About" should no longer be removed when dev mode is disabled in the settings</br>* Box shots and for channel previews should now be displayed properly'
       },
       thumbStyle: { right: '4px', borderRadius: '5px', background: 'white', width: '7px', opacity: 0.7 },
       contentStyle: { background: 'sedondary' }, // used when cursor is NOT over chat area
